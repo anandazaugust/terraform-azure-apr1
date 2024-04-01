8203d80fe216b2dbc5a8881bd418156a8c4a9782
anandraowpp <anand.rao@wpp.com>
2024-04-01 06:53:38 +0530

    a

--- test/rg.tf
+++ test/rg.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "subfolder" {
   name     = "subfolder"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "subfolder2" {
+  name     = "subfolder2"
+  location = "West Europe"
+}
