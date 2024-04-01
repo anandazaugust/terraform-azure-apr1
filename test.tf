e0d9ebee16a23d8e22e158ae69501f351b92ce46
anandraowpp <anand.rao@wpp.com>
2024-04-01 06:06:22 +0530

    a

--- test.tf
+++ test.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "newtfrg"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example2" {
+  name     = "newtfrg2"
+  location = "West Europe"
+}
